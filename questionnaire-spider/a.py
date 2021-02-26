d341aee797debeebb533cea0bd466d8102d7b080
shui-dun <2604697844@qq.com>
2021-02-26 16:17:11 +0800

    update questionnaire

--- questionnaire-spider/a.py
+++ questionnaire-spider/a.py
@@ -2,29 +2,41 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 import random
 import time
+from clash_helper import switch_proxy
+from concurrent.futures import ThreadPoolExecutor
+
 
 # 自动填“问卷星”
 
-if __name__ == '__main__':
-    options = uc.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_experimental_option('mobileEmulation', {'deviceName': 'iPhone X'})
-    driver = uc.Chrome(options=options)
-    driver.get("https://www.wjx.cn/vm/PgndNFc.aspx")
-    divs = driver.find_elements(By.CSS_SELECTOR, '.field.ui-field-contain')
-    for div in divs:
-        ty = div.get_attribute("type")
-        if ty == "1":
-            inp = div.find_element(By.TAG_NAME, "input")
-            inp.send_keys("hello world")
-        elif ty == "3":
-            a = div.find_elements(By.CLASS_NAME, "jqradio")
-            random.choice(a).click()
-        elif ty == "4":
-            a = div.find_elements(By.CLASS_NAME, "jqcheck")
-            for i in range(len(a) // 2):
+def foo():
+    while True:
+        switch_proxy()
+        options = uc.ChromeOptions()
+        # options.add_argument('--headless')
+        options.add_experimental_option('mobileEmulation', {'deviceName': 'iPhone X'})
+        driver = uc.Chrome(options=options)
+        driver.get("https://www.wjx.cn/vm/PgndNFc.aspx")
+        divs = driver.find_elements(By.CSS_SELECTOR, '.field.ui-field-contain')
+        for div in divs:
+            ty = div.get_attribute("type")
+            if ty == "1":
+                inp = div.find_element(By.TAG_NAME, "input")
+                inp.send_keys("hello world")
+            elif ty == "3":
+                a = div.find_elements(By.CLASS_NAME, "jqradio")
                 random.choice(a).click()
-    time.sleep(1)
-    button = driver.find_element(By.ID, "ctlNext")
-    button.click()
-    driver.quit()
+            elif ty == "4":
+                a = div.find_elements(By.CLASS_NAME, "jqcheck")
+                for i in range(len(a) // 2):
+                    random.choice(a).click()
+        time.sleep(1)
+        button = driver.find_element(By.ID, "ctlNext")
+        button.click()
+        driver.quit()
+
+
+if __name__ == '__main__':
+    num = 16
+    with ThreadPoolExecutor(num) as pool:
+        for i in range(16):
+            pool.submit(foo)
---
+++ questionnaire-spider/clash_helper.py
@@ -0,0 +1,28 @@
+import requests
+import random
+
+url = 'http://127.0.0.1:60561/proxies/GLOBAL'
+lst = []
+
+
+# 使用clash的restful API，实现代理手动切换
+
+def get_proxies_list():
+    """
+    获取所有可用代理，存于lst中
+    """
+    global lst
+    proxies = requests.get(url)
+    jsn = proxies.json()
+    lst = jsn['all'][2:-3]
+
+
+def switch_proxy():
+    """
+    切换使用的代理服务器
+    """
+    if len(lst) == 0:
+        get_proxies_list()
+    proxy = random.choice(lst)
+    data = '{{"name": "{}"}}'.format(proxy).encode('utf-8')
+    requests.put(url, data=data)
